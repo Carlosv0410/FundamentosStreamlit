72d877043f406f09cecfb698cd9273fccf03a58a
Carlos Carrillo Villavicencio <69774730+Carlosv0410@users.noreply.github.com>
2026-06-27 08:54:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ st.sidebar.image("LogoUHE.png")
 
 st.sidebar.title("Parámetros")
 
+
+monto = st.number_input("Ingrese el monto de su prestamo")
+
+interes = st.number_input("Ingrese el interes")
+
+tiempo_meses st.number_input("Ingrese el tiempo en meses")
+
+resultado = monto*interes*(tiempo_meses/12)
+
+st.write("El resultado es:" , reultado)
